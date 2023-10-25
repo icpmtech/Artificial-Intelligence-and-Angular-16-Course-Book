fe05475d69fa15e91f4e51949f17b700a77ec911
Pedro Martins <mourao.martins@gmail.com>
2023-10-25 19:00:25 +0100

    update

--- 4-3-how-to-use-tensorflow-js-in-angular16/app-tensorflow-app/src/app/app.component.ts
+++ 4-3-how-to-use-tensorflow-js-in-angular16/app-tensorflow-app/src/app/app.component.ts
@@ -1,10 +1,21 @@
+// @ts-nocheck
 import { Component } from '@angular/core';
-
+import * as tf from '@tensorflow/tfjs';
+import * as tfvis from '@tensorflow/tfjs-vis';
+// @ts-nocheck/
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app-tensorflow-app';
+  async loadModel() {
+    const model = await tf.loadLayersModel('path/to/model.json');
+    // Perform operations with the loaded model
+  }
+
+  ngOnInit() {
+    runAdditionRNNDemo();
+  }
 }
